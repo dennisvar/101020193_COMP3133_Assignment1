3c3c476fc0279ca13c5347f890b712d89ee75479
Dennis Varghese <dennis.varghese@hotmail.com>
2023-02-19 22:11:58 -0500

    feat: add typeDefs to index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const User = require('./models/user');
+const Employee = require('./models/employee');
+
 mongoose.set('strictQuery', true)
 mongoose.connect(process.env.DB , {
     useNewUrlParser: true,
@@ -11,6 +16,56 @@ mongoose.connect(process.env.DB , {
 }).catch(error => {
     console.log("FAILED TO CONNECT??")
 })
+
+const typeDefs = gql`
+    type User {
+        username: String
+        email: String
+        password: String
+    }
+
+    type Employee {
+        _id: ID
+        first_name: String
+        last_name: String
+        email: String
+        gender: String
+        salary: Float
+    }
+
+    type Query {
+        login(
+            email: String, 
+            password: String
+        ): User
+        getAllEmployees: [Employee]
+        searchEmployeeByEid(_id: ID): Employee
+    }
+
+    type Mutation {
+        signup(
+            username: String, 
+            email: String, 
+            password: String
+        ): User
+        addNewEmployee(
+            first_name: String
+            last_name: String
+            email: String
+            gender: String
+            salary: Float
+        ): Employee
+        updateEmployeeByEid(
+            _id: ID
+            first_name: String
+            last_name: String
+            email: String
+            gender: String
+            salary: Float
+        ): Employee
+        deleteEmployeeByEid(_id: String): Employee
+    }
+`;
 
 const app = express()
 

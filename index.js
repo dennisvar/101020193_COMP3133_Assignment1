66f376d7524a476b0ea8911429b1f6c041916c61
Dennis Varghese <dennis.varghese@hotmail.com>
2023-02-19 22:13:21 -0500

    feat: add resolvers to index.js

--- index.js
+++ index.js
@@ -67,6 +67,59 @@ const typeDefs = gql`
     }
 `;
 
+const resolvers = {
+    Query: {
+        login: async (parent, args) => {
+            const { email, password } = args;
+        },
+
+        getAllEmployees: async () => {
+            return Employee.find({});
+        },
+
+        searchEmployeeByEid: async (parent, args) => {
+            return Employee.findById(args._id)
+        }
+    },
+
+    Mutation: {
+        signup: async (parent, args) => {
+            let newUser = new User({
+                username: args.username,
+                email: args.email,
+                password: args.password
+            })
+
+            return newUser.save()
+        },
+
+        addNewEmployee: async (parent, args) => {
+
+
+            let newEmployee = new Employee({
+                first_name: args.first_name,
+                last_name: args.last_name,
+                email: args.email,
+                gender: args.gender,
+                salary: args.salary
+            })
+
+            return newEmployee.save()
+        },
+
+        updateEmployeeByEid: async (parent, args) => {
+            let updatedEmployee = await Employee.findById(args._id, args.input)
+
+            return updatedEmployee.save();
+        },
+        deleteEmployeeByEid: async (parent, args) => {
+            let deletedEmployee = await Employee.findByIdAndDelete(args._id)
+            
+            return deletedEmployee;
+        }
+    }
+}
+
 const app = express()
 
 app.listen(process.env.PORT, () => {
